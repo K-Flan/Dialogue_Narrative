977a75e841a5e02f441f18207f7d15658d9504c5
k <kflanagan363@gmail.com>
2020-10-20 22:29:59 +0100

    updated script

--- vector_semantics.py
+++ vector_semantics.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from itertools import chain
 
 class TDMatrix:
 
@@ -7,19 +8,23 @@ class TDMatrix:
         """ Initialise documents and document names"""
         self.documents = documents
         self.doc_names = doc_names
-        self.count = []
         
     
     def term_document_matrix(self):
         """ Create a term document matrix for a set of documents """
-        self.vocab = list(set([w for d in self.documents for w in d]))
+        
+        # create vocab set
+        self.vocab = set(chain.from_iterable(self.documents))
+        
+        # initialise final matrix with zeros
+        self.td_matrix = np.zeros((len(self.vocab),len(self.documents)))
         
         # count frequency of each word in each document
-        for i in range(len(self.documents)):
-            self.count.append([self.documents[i].count(w) for w in self.vocab])
+        for i,w in enumerate(self.vocab):
+            self.td_matrix[i,:] = [d.count(w) for d in self.documents]
 
-        df = pd.DataFrame(self.count, self.doc_names, self.vocab)
-        df = df.transpose()
+        df = pd.DataFrame(self.td_matrix, self.vocab, self.doc_names)
+        
         return df
 
 
@@ -29,26 +34,30 @@ class TF_IDF_Matrix:
         """ Initialise documents and document names"""
         self.documents = documents
         self.doc_names = doc_names
-        self.word_freq_count = []
     
     def tf_idf_matrix(self):
         """Create a tf-idf weighted matrix"""
-        self.vocab = list(set([w for d in self.documents for w in d]))
+        
+        # create vocab set
+        self.vocab = set(chain.from_iterable(self.documents))
+        
         # initialise final matrix with zeros
         self.tf_idf_mat = np.zeros((len(self.vocab),len(self.documents)))
 
         # cycle through each word in vocabulary to get frequency per document
         for i,w in enumerate(self.vocab):
-            self.word_freq_count = np.array([d.count(w) for d in self.documents])
+            self.term_freq = np.array([d.count(w) for d in self.documents])
             
             # how many documents each word is in
-            self.word_doc_count = 0
+            self.doc_freq = 0
             for d in self.documents:
                 if w in d:
-                    self.word_doc_count+=1
+                    self.doc_freq += 1
 
             # calculate tf-idf            
             self.calculate_tf_idf()
+            
+            # assign tf-idf values to the corresponding matrix row
             self.tf_idf_mat[i,:] = self.tf_idf
 
             tf_idf_df = pd.DataFrame(self.tf_idf_mat, self.vocab, self.doc_names)
@@ -57,8 +66,8 @@ class TF_IDF_Matrix:
     
     def calculate_tf_idf(self):
         """Calculate tf and idf and multiply to get tf-idf weighted values"""
-        self.tf = np.log10(self.word_freq_count + 1)
-        self.idf = np.log10(len(self.documents)/self.word_doc_count)
+        self.tf = np.log10(self.term_freq + 1)
+        self.idf = np.log10(len(self.documents)/self.doc_freq)
 
         self.tf_idf = self.tf*self.idf
         
@@ -77,22 +86,23 @@ class WordMatrix:
     
     def word_matrix(self):
         """Create a word-word matrix for a single document"""
+        
         # Find the locations of each unique word in the document
-        for i in set(self.document):
-            word_locs = np.where(self.document_array == i)[0]
+        for w in set(self.document):
+            word_locs = np.where(self.document_array == w)[0]
             
             # Put all context words for this word into a list
             self.contexts = []
             for j in word_locs:
-                self.context = self.document[max(j-self.window_size,0):j] + \
-                self.document[j+1:min(j+1+self.window_size,len(self.document))]
+                self.context = self.document[max(j-self.window_size, 0):j] + \
+                self.document[j+1:min(j+1+self.window_size, len(self.document))]
                 self.contexts += self.context
                 
                 # Get counts for each context word
                 self.get_context_counts()
             
             # Set the context counts vector to the corresponding row in the word-word matrix
-            vocab_word_index = np.where(np.array(self.vocab) == i)[0]
+            vocab_word_index = np.where(np.array(self.vocab) == w)[0]
             self.word_word_matrix[vocab_word_index,:] = self.context_vector
             
         df_context = pd.DataFrame(self.word_word_matrix, self.vocab, self.vocab)
